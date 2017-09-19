98094067902a125916fc260c5262bc2ffe20c962
aSamurai <xhc044x@gmail.com>
2017-09-20 01:56:05 +0300

    Optimize urlFormatter

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,5 @@
 import config from './../../config';
 
-const urlFormatter = url => {
-    return `${config.global.api.url}/${url}`;
-};
+const urlFormatter = url => `${config.global.api.url}/${url}`;
 
 export default urlFormatter;
