84989696044ae40ec1bdbdd4e8ec9142426e201b
aSamurai <xhc044x@gmail.com>
2017-10-09 23:43:02 +0300

    Add eslintignore folders andd add one more strict rule to eslint

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -16,24 +16,18 @@ export default function (state = initialState, action){
         case COUNT_REMOVE.REQUEST:
         case COUNT_CLEAR.REQUEST:
             return {...state, error: null, loading: true};
-            break;
         case COUNT_ADD.SUCCESS:
             return {...state, error: null, loading: false, count: state.count+1 };
-            break;
         case COUNT_REMOVE.SUCCESS: 
             return {...state, error: null, loading: false, count: state.count-1 };
-            break;
         case COUNT_CLEAR.SUCCESS: 
             return {...state, error: null, loading: false, count: 0 };
-            break;
         case COUNT_ADD.ERROR: 
         case COUNT_REMOVE.ERROR:
         case COUNT_CLEAR.ERROR:
             return {...state, error: action.error, loading: false};
-            break;  
         default:
             return state;
-            break;
     }
 }
 
